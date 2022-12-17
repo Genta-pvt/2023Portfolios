93c06ae96c4324e0222f004ba536f6c034440f3c
Genta1215 <popopoz0410@gmial.com>
2022-12-17 23:56:44 +0900

    add comments

--- saitama_neko/se_cats_schedule.py
+++ saitama_neko/se_cats_schedule.py
@@ -1,10 +1,16 @@
-import schedule
-import time
-import saitama_se_cats
+# ファイル名 : se_cats_schedule.py
+# 機能概要 : 定期的にsaitama_se_cats.pyを実行する
 
+# ライブラリ、モジュールをインポート
+import schedule  # スケジュール
+import time  # 時刻
+import saitama_se_cats  # saitama_se_cats
+
+# 実行時刻
 run_time = '12:00'
+# スケジュール設定 毎日run_timeになったら
 schedule.every().day.at(run_time).do(saitama_se_cats.CreateCatlist)
-
+# スケジュールスタート
 while True :
     schedule.run_pending()
     time.sleep(30)
