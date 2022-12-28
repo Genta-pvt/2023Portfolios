9e49b75001d7e1a6a0d92719012896c7f28101a8
Genta1215 <popopoz0410@gmial.com>
2022-12-28 18:33:21 +0900

    Resolve isuue #14

--- saitama_neko/SaitamaSeCats.py
+++ saitama_neko/SaitamaSeCats.py
@@ -95,65 +95,6 @@ class SaitamaCats:
 
 
 
-
-# 未使用クラス レビュー不要
-class ImportCatData:
-    def create_catlist():
-        # 変数定義
-        cats_data = []  # すべての猫(<table>)のデータ
-        each_table = []  # <table>タグ内のデータを一時的に格納
-        # HTMLデータの取得
-        r = requests.get('https://www.pref.saitama.lg.jp/b0716/joutoseineko-s.html')
-        soup = BeautifulSoup(r.content,'html.parser')
-
-        # 猫のデータを配列に格納
-        # すべての<table>(猫情報)で繰り返し
-        for t in soup.find_all('table'):
-            # すべての<tr>(列)で繰り返し
-            for tr in t.find_all('tr'):
-                # データ行(2行目)の<td>(セル)のデータをeach_valueに代入
-                each_value = tr.find_all('td')[1]
-                # each_valueに<p>タグがある時(セル内改行があるとき)
-                if x := each_value.find_all('p'):
-                    # セル内行をそれぞれstrにしてパッキング。それをeach_tableに追記(空白のセル内行を除く)
-                    each_table.append([y.text for y in x if not re.fullmatch(r'[\s]+',y.text)])
-                # セル内改行がないとき
-                else:
-                    # テキストをeach_tableに追記
-                    each_table.append(each_value.text.replace('\n',''))
-            # cats_dataにeach_tableの内容を追記
-            cats_data.append(each_table[:])
-            # each_tableを初期化
-            each_table.clear()
-        # 実行時の日付 + 実行時に何匹里親募集状態の猫がいるかのstrを戻り値に
-        message = datetime.date.today().strftime('%Y年%m月%d日') + ' 現在、埼玉県南部・東部地区では' + f'{len(cats_data):2}' + '匹の猫が里親を募集しています'
-        return message
-
-# 未使用関数 レビュー不要
-def send_mail():
-    # 定数初期化
-    SERVER ='smtp.gmail.com'
-    FROM = 'zopopop0140@gmail.com'
-    TO = 'zopopop0140@gmail.com'
-    PASS = 'odfivqnnquytcxlr'
-
-    # メール作成
-    mail = MIMEText('hoge')
-    mail['Subject'] = '本日の埼玉県南部・東部地区における猫の里親募集状況です'
-    mail['From'] = FROM
-    mail['To'] = TO
-
-    # メール送信
-    with smtplib.SMTP(SERVER,587) as smtp:
-        smtp.ehlo()
-        try:
-            smtp.starttls()
-            smtp.ehlo
-        except smtplib.SMTPNotSupportedError:
-            pass
-        smtp.login('zopopop0140','mblqqixlbsfgveid')
-        smtp.sendmail(FROM, TO, mail.as_string)
-
 # 単体で実行したときの処理
 if __name__ == '__main__':
     pass
