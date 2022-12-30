3a89080be62742557d819410eb0f38230f62781b
Genta1215 <popopoz0410@gmial.com>
2022-12-30 17:06:39 +0900

    edit OperationTweetBot

--- saitama_neko/OperationTweetBot.py
+++ saitama_neko/OperationTweetBot.py
@@ -1,17 +1,22 @@
 import tweepy
 import SaitamaSeCats
-
+import TwCredentials
 
 
 # ツイートするクラス
 class OperationTweetBot():
-    def __init__(self,consumer_key,consumer_secret,bearer_token,access_token,access_secret):
-        self.consumer_key = consumer_key
-        self.consumer_secret = consumer_secret
-        self.bearer_token = bearer_token
-        self.access_token = access_token
-        self.access_secret = access_secret
-        self.client = tweepy.Client(bearer_token = self.bearer_token, consumer_key = self.consumer_key, consumer_secret = self.consumer_secret, access_token = self.access_token, access_token_secret = self.access_secret)
+    def __init__(self):
+        # self.consumer_key = consumer_key
+        # self.consumer_secret = consumer_secret
+        # self.bearer_token = bearer_token
+        # self.access_token = access_token
+        # self.access_secret = access_secret
+        self.credentials = TwCredentials.SaitamaCatInf()
+        self.client = tweepy.Client(bearer_token = self.credentials.bearer_token, \
+                                    consumer_key = self.credentials.consumer_key, \
+                                    consumer_secret = self.credentials.consumer_secret, \
+                                    access_token = self.credentials.access_token, \
+                                    access_token_secret = self.credentials.access_token)
 
     
     def tweet(self,value):
@@ -25,4 +30,5 @@ if __name__ == '__main__':
     BEARER_TOKEN = ''
     ACCESS_TOKEN = ''
     ACCESS_SECRET = ''
-    OperationTweetBot(CONSUMER_KEY,CONSUMER_SECRET,BEARER_TOKEN,ACCESS_TOKEN,ACCESS_SECRET).tweet(SaitamaSeCats.CreateSentens('s').sentens_2())
+    OperationTweetBot().tweet(SaitamaSeCats.CreateSentens('s').sentens_2())
+    # print(Credentials.SaitamaCatInf().consumer_key)
--- saitama_neko/credlib.py
+++
@@ -1,5 +0,0 @@
-class credential:
-    def __init__(self, hostname, username, password):
-        self.hostname = hostname
-        self.username = username
-        self.password = password
