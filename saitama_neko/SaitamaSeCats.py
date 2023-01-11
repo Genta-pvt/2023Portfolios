40f764b4c02650512e778ebcfc91c66d4c97016f
Genta1215 <popopoz0410@gmial.com>
2023-01-11 12:43:30 +0900

    solve #24

--- saitama_neko/SaitamaSeCats.py
+++ saitama_neko/SaitamaSeCats.py
@@ -19,10 +19,10 @@
 #       create_catlist()を実行し、戻り値をメールで送信する
 
 # ライブラリをインポート
-from bs4 import BeautifulSoup   # HTML解析
 import requests  # HTML取得
 import re  # 正規表現
 import datetime  # 時刻
+from bs4 import BeautifulSoup   # HTML解析
 
 
 
--- saitama_neko/test1.py
+++ saitama_neko/test1.py
@@ -1,6 +1,6 @@
-from bs4 import BeautifulSoup
 import requests
 import re
+from bs4 import BeautifulSoup
 
 def CreateCatlist(url):
     r = requests.get(url)
