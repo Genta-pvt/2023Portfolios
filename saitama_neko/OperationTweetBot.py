78e3e6a8f24feddf275b3b35d22ccbdc6da4d1f8
Genta1215 <popopoz0410@gmial.com>
2023-01-19 23:23:11 +0900

    クレデンシャルを初期化する機能を追加中

--- saitama_neko/OperationTweetBot.py
+++ saitama_neko/OperationTweetBot.py
@@ -8,22 +8,23 @@ import tweepy
 # import CreateSentens
 import TwittetAPICredentials
 
-class OperationTweetBot():
+
+class OperationTweetBot:
     """
     Twitterクライアントを取り扱うクラス
     """
+
     def __init__(self):
         """
         処理
             外部からインポートした認証情報を利用してTwitterクライアントを初期化
         """
         self.credentials = TwittetAPICredentials.SaitamaCatInf()
-        self.client = tweepy.Client(bearer_token = self.credentials.bearer_token, \
-                                    consumer_key = self.credentials.consumer_key, \
-                                    consumer_secret = self.credentials.consumer_secret, \
-                                    access_token = self.credentials.access_token, \
-                                    access_token_secret = self.credentials.access_secret)
-
+        self.client = tweepy.Client(bearer_token=self.credentials.bearer_token,
+                                    consumer_key=self.credentials.consumer_key,
+                                    consumer_secret=self.credentials.consumer_secret,
+                                    access_token=self.credentials.access_token,
+                                    access_token_secret=self.credentials.access_secret)
 
 
 def tweet(value):
@@ -36,7 +37,8 @@ def tweet(value):
     """
     # OperationTweetBot.client.create_tweet(text = value)
     sci_client = OperationTweetBot()
-    sci_client.client.create_tweet(text = value)
+    sci_client.client.create_tweet(text=value)
+
 
 # 単体で実行したときの処理
 if __name__ == '__main__':
--- saitama_neko/RunSchedule.py
+++ saitama_neko/RunSchedule.py
@@ -1,5 +1,5 @@
 """
-ファイル名 : CatsSchedule.py
+ファイル名 : RunSchedule.py
 ■概要
 処理を定期実行するためのモジュールです
 """
@@ -15,8 +15,8 @@ def day_task():
     処理
         譲渡用猫情報の各ページ(北西部、南東部)の情報をまとめたツイートをする
     """
-    AREA_LIST = ['nw', 'se']
-    for area in AREA_LIST:
+    area_list = ['nw', 'se']
+    for area in area_list:
         tweet(CreateSentens(area).sentens_2())
 
 
